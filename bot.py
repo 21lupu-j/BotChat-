b66395d9f5621c8619d8e57ba684f27d74c3be99
21lupu-j <46443840+21lupu-j@users.noreply.github.com>
2019-01-21 12:22:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,56 +65,78 @@ class ContextChat(Chat):
 
 #money related questions
 
+user_name = "you"
 
-
-answers = ['over 9000']
-
+def define_user_name(name):
+    user_name = name.strip()
+    return "It is nice to meet you, " + user_name + "!"
 
 
 pairs = [
+    [
+        #name question #1
+        r'(.*)(name) (is)(.*)',
+        [lambda matches: define_user_name(matches[3])],
+    ],
+
+    [
+        #name question #2
+        r"(I am|I'm)(.*)",
+        [lambda matches: define_user_name(matches[1])],
+    ],
+
+    [
+        #visits/open house related questions
+        r'(.*) (visit|visiting|open house) (without) (.*)',
+        ['No, we ask all guests to make an appointment with admissions before arriving at the school'],
+    ],
 
     [
         #visits/open house related questions
         r'(.*)(visit|visiting|open house)(.*)',
-        ['visit us whenever :D'],
+        ['We can be contacted to schedule an individual visit. There are also open houses held at the school, which we recommend for visiting the school. To find out more about the Open houses and how to contact for visiting the school please follow the following link to find a registration form to attend the open house and on more information on how to schedule a visit. https://www.aswarsaw.org/admissions/visit'],
+    ],
+
+    [
+        #money related questions
+        r'(I|.*)(cost|fee|price|pay|tuition)(.*)',
+        ['The school requires a registration fee. The yearly tuition of the school varies from the Pre-Kindergarten tuition which is 5,500$ + PLN 28,200, to the seniors tuition which is 9,900$ + PLN 56,000, to find out more in detail what the different tuitions are for the different years we recommend exploring the Tuitions and Applications Fees site.  Here is the link directly to the site - https://www.aswarsaw.org/admissions/school-fees'],
+    ],
+
+    [
+        #admissions
+        r'(.*)(apply|admitted|application)(.*)',
+        ['You can apply to our school and check your status using the OpenApply website at: https://aswarsaw.openapply.com/'],
     ],
     
     [
-        #money related questions
-        r'(whats|what\'s|what is|what|how|do) (.*) (cost|fee|price|pay|tuition)',
-        ['The yearly tuition of the school varies from the Pre-Kindergarten tuition which is 5,500$ + PLN 28,200, to the seniors tuition which is 9,900$ + PLN 56,000, to find out more in detail what the different tuitions are for the different years we recommend exploring the Tuitions and Applications Fees site.  Here is the link directly to the site - https://www.aswarsaw.org/admissions/school-fees'],
-    ],
-
-    [
         #contact to the school related questions
         r'(what is|what\'s|what|where|how)(.*)(contact|email|phone number|contacts)(|school)',
-        ['We have multiple phone numbers and emails depending on who you want to contact, all school offices have both a phone number and email for you to use.\nWe recommend using the contact page to find further information on how to contact us and to find specific phone numbers and emails for the different offices available.\nClick the following link to go to the contacts page - https://www.aswarsaw.org/contact'],
+        ['We have multiple phone numbers and emails depending on who you want to contact, all school offices have both a phone number and email for you to use. We recommend using the contact page to find further information on how to contact us and to find specific phone numbers and emails for the different offices available. Click the following link to go to the contacts page - https://www.aswarsaw.org/contact. If you are thinking about applying, you should first get in touch with the admissions department to find out more about the school and arrange a visit'],
     ],
 
     [
-        #location of the school related questions
-        r'(what is|what\'s|what|where|how)(.*)(campus|school|get to|addresss|located)(.*)',
-        ['The address of the school is: Warszawska 202, 05-520 Bielawa', 'The school is located a little outside of Warsaw, at: Warszawska 202, 05-520 Bielawa', 'You can find the campus at Warszawska 202, 05-520 Bielawa'],
-    ],
-
-    [
+         #cafeteria related questions
        r'(Is|Do|How|Where|What)(.*)(food|eat|cafeteria|canteen)(.*)',
        ['Food is prepared on-site with meat and vegetarian options. These meals, prepared with care, consist of Polish and international cuisines. If you have any questions, please contact Soliwoda Catering via email at asw@soliwoda.pl.'],
     ],
 
     [
-       r'(Can|How|Where|What|Is)(.*)(photos|images|pictures|look|nice)(.*)',
+        #campus related questions
+       r'(Can|How|Where|What|Is|Does|I)(.*)(photos|images|pictures|look|nice|like)(.*)',
        ['You can see the pictures of the campus here: https://www.aswarsaw.org/about-us/campus', "Please see our campus on the website: https://www.aswarsaw.org/about-us/campus"],
     ],
 
-
-
-
+    [
+        #location of the school related questions
+        r'(what is|what\'s|what|where|how|)(.*)(campus|school|get to|addresss|located)(.*)',
+        ['The address of the school is: Warszawska 202, 05-520 Bielawa', 'The school is located a little outside of Warsaw, at: Warszawska 202, 05-520 Bielawa', 'You can find the campus at Warszawska 202, 05-520 Bielawa'],
+    ],
 
 ]
 
 if __name__ == "__main__":
-    print("Hello, ask me if you need any help finding something. I will try my best to answer questions.")
+    print("Hello, ask me if you need any help finding something. I will try my best to answer questions. What is your name?")
     chat = ContextChat(pairs, reflections)
     chat.converse()
     
